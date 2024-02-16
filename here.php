0da0a48a69980488056728bc0bf7660ccab128c2
GrimDevxlx <slj1999x@gmail.com>
2024-02-16 15:44:16 +0800

    another querrrrry

--- here.php
+++ here.php
@@ -2,6 +2,18 @@
   session_start();
   require 'dbcon.php';
 
+  if(isset($_POST['update_patient']))
+  {
+    $patient_id = mysqli_real_escape_string($con, $_POST['patient_id']);
+    $name = mysqli_real_escape_string($con, $_POST['name']);
+    $email = mysqli_real_escape_string($con, $_POST['email']);
+    $phone = mysqli_real_escape_string($con, $_POST['phone']);
+    $address = mysqli_real_escape_string($con, $_POST['address']);
+
+    $query = "UPDATE SET patients name='$name', email='$email', phone='$phone', address='$address' WHERE id ='$patient_id' ";
+  }
+
+
   if(isset($_POST['save_patient']))
   {
         $name = mysqli_real_escape_string($con, $_POST['name']);
--- patient-edit.php
+++ patient-edit.php
@@ -34,32 +34,42 @@
                        $patient_id = mysqli_real_escape_string($con, $_GET['id']);
                        $query = "SELECT * FROM patients WHERE id ='$patient_id' ";
                        $query_run = mysqli_query($con, $query);
-                    }
-                  ?>
 
-                <form action="here.php" method="POST">
+                       if(mysqli_num_rows($query_run) > 0)
+                       {
+                          $patient = mysqli_fetch_array($query_run);
+                          ?>
+                     
+                      <form action="here.php" method="POST">
 
                   <div class="mb-3">
                   <label>Patient Name:</label>
-                  <input type="text" name="name" placeholder="John doe" class="form-control">
+                  <input type="text" name="name" value="<?=$patient['name']; ?>" placeholder="John doe" class="form-control">
                   </div>
                   <div class="mb-3">
                   <label>Patient Email:</label>
-                  <input type="email" name="email" placeholder="@yahoo/@gmail" class="form-control">
+                  <input type="email" name="email"  value="<?=$patient['email']; ?>" placeholder="@yahoo/@gmail" class="form-control">
                   </div>
                   <div class="mb-3">
                   <label>Patient Phone:</label>
-                  <input type="text" name="phone"  placeholder="639+" class="form-control">
+                  <input type="text" name="phone"   value="<?=$patient['phone']; ?>" placeholder="639+" class="form-control">
                   </div>
                   <div class="mb-3">
                   <label>Patient Address:</label>
-                  <input type="text" name="address" placeholder="PHL" class="form-control">
+                  <input type="text" name="address"  value="<?=$patient['address']; ?>" placeholder="PHL" class="form-control">
                   </div>
                   <div class="mb-3">
                     <button type="submit" name="update_patient" class="btn btn-primary">Update Patient</button>
                   </div>
-
-                </form>
+                      </form>
+                      <?php
+                       }
+                       else
+                       {
+                          echo "<h4>No Such Patient ID Found!</h4>";
+                       }
+                    }
+                  ?>
               </div>
           </div>
         </div>
