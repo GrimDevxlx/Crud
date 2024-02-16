0c0b1d3e9e1805666532887cb4067b4a4a6f38a9
GrimDevxlx <slj1999x@gmail.com>
2024-02-16 14:01:59 +0800

    CONNECT

--- here.php
+++ here.php
@@ -1,8 +1,13 @@
 <?php
   require 'dbcon.php';
 
-  if(isset($_POST[]))
+  if(isset($_POST['save_patient']))
   {
+        $name = mysqli_real_escape_string($con, $_POST['name']);
+        $email = mysqli_real_escape_string($con, $_POST['email']);
+        $phone = mysqli_real_escape_string($con, $_POST['phone']);
+        $address = mysqli_real_escape_string($con, $_POST['address']);
 
+        $query = "INSERT INTO patients (name,email,phone,address) VALUES ()";
   }
 ?>
