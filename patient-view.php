3da3fefdc19e3f6f0995836dd159bf09dd1ef296
GrimDevxlx <slj1999x@gmail.com>
2024-02-16 16:51:28 +0800

    movedFolder

--- patient-view.php
+++ patient-view.php
@@ -1,22 +1,8 @@
 <?php 
     require 'dbcon.php';
 ?>
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-    <title>Patient Read Area</title>
-  </head>
-  <body>
-
+      <?php include('includes/header.php'); ?>
     <div class="container mt-5">
-
-
       <div class="row">
         <div class="col-md-12">
           <div class="card">
@@ -81,11 +67,4 @@
       </div>
     </div>
 
-   
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-  
-  </body>
-</html>
+    <?php include('includes/footer.php'); ?>
